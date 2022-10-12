7ff76124c5b736111aa78b3087e309b86b1b7e72
ningshuohao <ningshuohao@163.com>
2022-10-12 13:10:52 +0800

    Update application.cpp

--- cpp-code/app-a/application.cpp
+++ cpp-code/app-a/application.cpp
@@ -4,16 +4,7 @@
 #include <fstream>
 using namespace std;
 int main(int argc, char **argv){
-    if (argc == strlen(argv[0])){
-        for (;;){
-            string inputStr = "";
-            cout << ">>>";
-            cin >> inputStr;
-        };
-        cout << endl;
-    }else{
-        if (argv[1] == "/r"){
-            cout << "------Getting------";
-        };
+    if (argv[1] == "/r"){
+        cout << "------Getting------";
     };
 };
