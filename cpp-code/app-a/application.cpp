ad6d3ddd7e5da5aa078cc4ce73abe5bde9a64dc6
ningshuohao <ningshuohao@163.com>
2022-10-12 13:25:45 +0800

    Update application.cpp

--- cpp-code/app-a/application.cpp
+++ cpp-code/app-a/application.cpp
@@ -8,14 +8,15 @@ int main(){
     cout << "Test Application,version 0.1" << endl << "Mode 1:Open file to use." << endl << "Mode 2:Enter command.";
     cout << "Mode(1/2):";
     cin >> appMode;
-
-    for (;;){
-        string inputStr = "";
-        cout << ">>>";
-        cin >> inputStr;
-        if (inputStr == "exit"){
-            break;
+    if (appMode == "2"){
+        for (;;){
+            string inputStr = "";
+            cout << ">>>";
+            cin >> inputStr;
+            if (inputStr == "exit"){
+                break;
+            };
         };
+        cout << endl;
     };
-    cout << endl;
 };
