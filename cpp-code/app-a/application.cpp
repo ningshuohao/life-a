ee1f7d3b7904e8e01cbfe536b2a72f0ea17be453
ningshuohao <ningshuohao@163.com>
2022-10-12 13:24:29 +0800

    Update application.cpp

--- cpp-code/app-a/application.cpp
+++ cpp-code/app-a/application.cpp
@@ -4,6 +4,11 @@
 #include <fstream>
 using namespace std;
 int main(){
+    char appMode;
+    cout << "Test Application,version 0.1" << endl << "Mode 1:Open file to use." << endl << "Mode 2:Enter command.";
+    cout << "Mode(1/2):";
+    cin >> appMode;
+
     for (;;){
         string inputStr = "";
         cout << ">>>";
