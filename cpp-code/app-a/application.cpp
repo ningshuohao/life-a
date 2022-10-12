3b476322f67048253d85c38b265e192b53bdf829
ningshuohao <ningshuohao@163.com>
2022-10-12 13:12:26 +0800

    Update application.cpp

--- cpp-code/app-a/application.cpp
+++ cpp-code/app-a/application.cpp
@@ -11,6 +11,9 @@ int main(int argc, char **argv){
             string inputStr = "";
             cout << ">>>";
             cin >> inputStr;
+            if (inputStr == "exit"){
+                break;
+            };
         };
         cout << endl;
     };
